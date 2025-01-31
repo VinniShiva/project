1e571f7c455eabb82e5161cb293e210a3249926a
VinniShiva <shiva1234bandaru@gmail.com>
2025-01-31 22:52:28 +0530

    First commit

---
+++ SpringFirstApp/src/com/tcs/beans/Test.java
@@ -0,0 +1,10 @@
+package com.tcs.beans;
+
+public class Test {
+Test(){
+	System.out.println("Test");
+}
+public void wish() {
+	System.out.println("wish");
+}
+}
---
+++ SpringFirstApp/src/com/tcs/client/TestClient.java
@@ -0,0 +1,15 @@
+package com.tcs.client;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tcs.beans.Test;
+
+public class TestClient {
+public static void main(String[] args) {
+	ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+	System.out.println("&&&&&&");
+	Test t = (Test)context.getBean("test");
+	t.wish();
+}
+}
