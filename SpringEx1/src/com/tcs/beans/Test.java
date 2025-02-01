bf81798aa1fe0450bcc4d782d01cab8108de507c
VinniShiva <shiva1234bandaru@gmail.com>
2025-02-01 13:02:56 +0530

    commit

---
+++ SpringEx1/src/com/tcs/beans/Test.java
@@ -0,0 +1,14 @@
+package com.tcs.beans;
+
+public class Test {
+  Test(){
+	  System.out.println("constructor");
+  }
+  public void wish1() {
+	  System.out.println("hello");
+  }
+public void wish() {
+	System.out.println("hello");
+	
+}
+}
